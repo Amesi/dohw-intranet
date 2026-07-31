476bbecf4cd40a6bf812f0b6a42d8a0e65c06ecc
kumi <victemokang11@gmail.com>
2026-07-31 08:01:36 +0000

    fix: add auth guard to directory page

--- dohw_intranet/www/directory.py
+++ dohw_intranet/www/directory.py
@@ -5,6 +5,10 @@ import frappe
 
 def get_context(context):
     context.no_cache = 1
+    if frappe.session.user == "Guest":
+        frappe.local.flags.redirect_location = "/login?redirect-to=/directory"
+        raise frappe.Redirect
+
     context.show_sidebar = 1
     context.title = "Staff Directory"
 
