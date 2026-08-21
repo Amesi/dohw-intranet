b250f593d7282252d1b4d9c5786be3ee0208338d
DoWH Ops <ops@kumi-tech.com>
2026-08-21 20:22:48 +1000

    fix: remove inner sidebars from /directory (frappe portal sidebar + empty wing-filter tree)

--- dohw_intranet/www/directory.py
+++ dohw_intranet/www/directory.py
@@ -9,7 +9,7 @@ def get_context(context):
         frappe.local.flags.redirect_location = "/login?redirect-to=/directory"
         raise frappe.Redirect
 
-    context.show_sidebar = 1
+    context.show_sidebar = 0
     context.title = "Staff Directory"
 
     # Filters
